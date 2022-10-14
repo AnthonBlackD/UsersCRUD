713b84af5bdf238a27c63cdf4a80b31081c9b75d
AnthonBlackD <aremigioh1500@alumno.ipn.mx>
2022-10-14 09:56:39 -0500

    Se eliminan fotos del servidor, junto con los datos.

--- delete.php
+++ delete.php
@@ -1,24 +1,34 @@
 <?php
     
     $id=$_GET['id'];
-    $image=$_GET['image'];
+    
     include("db.php");
     
       
-    $sql="delete from usuarios where id='".$id."'";
-    $resultado=mysqli_query($conn,$sql);
-   
+    $sql="select * from usuarios where id='".$id."'";
+        $resultado=mysqli_query($conn,$sql);
+
+        $fila=mysqli_fetch_assoc($resultado);
+        $nombre=$fila['nombre'];
+        $apellido=$fila['apellido'];
+        $email=$fila['email'];
+        $password=$fila['pass'];
+        $img=$fila['image'];
+
+     
     
 
     if($resultado){
-        unlink($image);
+        $sql="delete from usuarios where id='".$id."'";
+        $res=mysqli_query($conn,$sql);
+        unlink($img);
         echo "<script language='JavaScript'>
                     alert('Los datos se eliminaron correctamente de la Base de Datos');
                     location.assign('index.php');
                     </script>";
     }
     else{
-        echo "<script language='JavaScript'>
+        echo $image."<script language='JavaScript'>
                     alert('Los datos NO se eliminaron de la Base de Datos');
                     location.assign('index.php');
                     </script>";
